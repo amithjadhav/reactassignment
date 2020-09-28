004efc6b753c5fa18e6e50121bd54499e5d64beb
amithjadhav <72000518+amithjadhav@users.noreply.github.com>
2020-09-28 16:50:47 +0530

    Updates

---
+++ precache-manifest.f16fd51dc8ffc0b27ed1e57a3c97b2d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6c36f5989036846fcec131541a512d",
+    "url": "/reactassignment/index.html"
+  },
+  {
+    "revision": "2e3038a00136030d22e2",
+    "url": "/reactassignment/static/css/main.86e86870.chunk.css"
+  },
+  {
+    "revision": "be27eb912b06bc28127d",
+    "url": "/reactassignment/static/js/2.a389078b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactassignment/static/js/2.a389078b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3038a00136030d22e2",
+    "url": "/reactassignment/static/js/main.a7d73c43.chunk.js"
+  },
+  {
+    "revision": "473194140ca7ec0008f5",
+    "url": "/reactassignment/static/js/runtime-main.03c5a305.js"
+  }
+]);
